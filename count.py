fa2a493ff84a981025e699da32a9db0718add460
Trav Mason <travis.mason@gmail.com>
2022-10-26 15:54:03 +1100

    adding LDA for topic modelling

--- count.py
+++ count.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import re
 
 import matplotlib.pyplot as plt
 
@@ -26,6 +27,88 @@ dir = 'gpt3_logs'
 #             #text += sto[1]
 #             print('Length (lines): %s' % len(sto[1].splitlines()))
 
+def topic_model(text):
+    #measure how well a conversation sticks to topic
+    # Load the regular expression library
+
+    # Remove punctuation
+    text['paper_text_processed'] = \
+    text['paper_text'].map(lambda x: re.sub('[,\.!?]', '', x))
+    # Convert the titles to lowercase
+    text['paper_text_processed'] = \
+    text['paper_text_processed'].map(lambda x: x.lower())
+    # Print out the first rows of papers
+    text['paper_text_processed'].head()
+
+    # Import the wordcloud library
+    from wordcloud import WordCloud
+    # Join the different processed titles together.
+    long_string = ','.join(list(papers['paper_text_processed'].values))
+    # Create a WordCloud object
+    wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
+    # Generate a word cloud
+    wordcloud.generate(long_string)
+    # Visualize the word cloud
+    wordcloud.to_image()
+
+    import gensim
+    from gensim.utils import simple_preprocess
+    import nltk
+    nltk.download('stopwords')
+    from nltk.corpus import stopwords
+    stop_words = stopwords.words('english')
+    stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+    def sent_to_words(sentences):
+        for sentence in sentences:
+            # deacc=True removes punctuations
+            yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))
+    def remove_stopwords(texts):
+        return [[word for word in simple_preprocess(str(doc)) 
+                if word not in stop_words] for doc in texts]
+    data = text.paper_text_processed.values.tolist()
+    data_words = list(sent_to_words(data))
+    # remove stop words
+    data_words = remove_stopwords(data_words)
+    print(data_words[:1][0][:30])
+
+    import gensim.corpora as corpora
+    # Create Dictionary
+    id2word = corpora.Dictionary(data_words)
+    # Create Corpus
+    texts = data_words
+    # Term Document Frequency
+    corpus = [id2word.doc2bow(text) for text in texts]
+    # View
+    print(corpus[:1][0][:30])
+
+    from pprint import pprint
+    # number of topics
+    num_topics = 10
+    # Build LDA model
+    lda_model = gensim.models.LdaMulticore(corpus=corpus,
+                                        id2word=id2word,
+                                        num_topics=num_topics)
+    # Print the Keyword in the 10 topics
+    pprint(lda_model.print_topics())
+    doc_lda = lda_model[corpus]
+
+    import pyLDAvis.gensim
+    import pickle 
+    import pyLDAvis
+    # Visualize the topics
+    pyLDAvis.enable_notebook()
+    LDAvis_data_filepath = os.path.join('./results/ldavis_prepared_'+str(num_topics))
+    # # this is a bit time consuming - make the if statement True
+    # # if you want to execute visualization prep yourself
+    if 1 == 1:
+        LDAvis_prepared = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
+        with open(LDAvis_data_filepath, 'wb') as f:
+            pickle.dump(LDAvis_prepared, f)
+    # load the pre-prepared pyLDAvis data from disk
+    with open(LDAvis_data_filepath, 'rb') as f:
+        LDAvis_prepared = pickle.load(f)
+    pyLDAvis.save_html(LDAvis_prepared, './results/ldavis_prepared_'+ str(num_topics) +'.html')
+    LDAvis_prepared
 
 filename = 'output.json'
 f = os.path.join(dir, filename)
@@ -35,7 +118,8 @@ print(df.dtypes)
 print(df.describe())
 for d in df["Response"]:
     print('Length (lines): %s' % len(d.splitlines()))
-    print("\n\n%s\n" % d)
+    #print("\n\n%s\n" % d)
+topic_model(d)
 
 # wordcloud = WordCloud(width=1000, height=500, stopwords=stopwords, max_words=500, background_color="white").generate(text)
 # plt.figure( figsize=(20,10), facecolor='k')
